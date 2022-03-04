68c9e6809eade7f679b5ef342517d5ec0c6da3f4
LAB-00\lab00-00 <e107ti@cps.sp.gov.br>
2022-03-04 18:57:39 -0300

    tarefa finalizada

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Guarujá!");
+Console.WriteLine("São Vicente!");
+Console.WriteLine("Santos!");
+Console.WriteLine("Mongaguá");
+Console.WriteLine("Itanhaém!");
+Console.WriteLine("Peruíbe!");
+Console.WriteLine("Praia Grande");
